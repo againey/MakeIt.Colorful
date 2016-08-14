d24ae018834a30326a3ac6564503b0770f7ec5b8
Andy Gainey <7810015+againey@users.noreply.github.com>
2016-08-14 14:50:51 -0500

    Added the five other color spaces to the demo scene controller, and fixed some bugs in the original code.

--- Assets/Experilous/Examples/MakeItColorful/Scripts/ColorSpacesController.cs
+++ Assets/Experilous/Examples/MakeItColorful/Scripts/ColorSpacesController.cs
@@ -27,11 +27,156 @@ namespace Experilous.Examples.MakeItColorful
 
 		protected void Start()
 		{
-			CalculateHueSliceHCY(0f, imageWidth, imageHeight, _colorSpaceSliceColors);
+			CalculateHueSliceHSY(0f, imageWidth, imageHeight, _colorSpaceSliceColors);
 
 			_colorSpaceSliceTexture.SetPixels(_colorSpaceSliceColors);
 			_colorSpaceSliceTexture.Apply();
 
+		}
+
+		private static void CalculateHueSliceHSV(float hue, int width, int height, Color[] colors)
+		{
+			float rx = width / 2 - 1f;
+			float ry = height - 1f;
+
+			float complementaryHue = Mathf.Repeat(hue + 0.5f, 1f);
+
+			for (int y = 0, i = 0; y < height; ++y)
+			{
+				float fy = y / ry;
+				for (int x = 0; x < width; ++x, ++i)
+				{
+					float fx = (x - rx) / rx;
+					if (fx < 0f)
+					{
+						var hsv = new ColorHSV(hue, -fx, fy);
+						hsv.a = 1f;
+						colors[i] = (Color)hsv;
+					}
+					else
+					{
+						var hsv = new ColorHSV(complementaryHue, fx, fy);
+						hsv.a = 1f;
+						colors[i] = (Color)hsv;
+					}
+				}
+			}
+		}
+
+		private static void CalculateHueSliceHCV(float hue, int width, int height, Color[] colors)
+		{
+			float rx = width / 2 - 1f;
+			float ry = height - 1f;
+
+			float complementaryHue = Mathf.Repeat(hue + 0.5f, 1f);
+
+			for (int y = 0, i = 0; y < height; ++y)
+			{
+				float fy = y / ry;
+				for (int x = 0; x < width; ++x, ++i)
+				{
+					float fx = (x - rx) / rx;
+					if (fx < 0f)
+					{
+						var hcv = new ColorHCV(hue, -fx, fy);
+						hcv.a = hcv.canConvertToRGB ? 1f : 0f;
+						colors[i] = (Color)hcv;
+					}
+					else
+					{
+						var hcv = new ColorHCV(complementaryHue, fx, fy);
+						hcv.a = hcv.canConvertToRGB ? 1f : 0f;
+						colors[i] = (Color)hcv;
+					}
+				}
+			}
+		}
+
+		private static void CalculateHueSliceHSL(float hue, int width, int height, Color[] colors)
+		{
+			float rx = width / 2 - 1f;
+			float ry = height - 1f;
+
+			float complementaryHue = Mathf.Repeat(hue + 0.5f, 1f);
+
+			for (int y = 0, i = 0; y < height; ++y)
+			{
+				float fy = y / ry;
+				for (int x = 0; x < width; ++x, ++i)
+				{
+					float fx = (x - rx) / rx;
+					if (fx < 0f)
+					{
+						var hsl = new ColorHSL(hue, -fx, fy);
+						hsl.a = 1f;
+						colors[i] = (Color)hsl;
+					}
+					else
+					{
+						var hsl = new ColorHSL(complementaryHue, fx, fy);
+						hsl.a = 1f;
+						colors[i] = (Color)hsl;
+					}
+				}
+			}
+		}
+
+		private static void CalculateHueSliceHCL(float hue, int width, int height, Color[] colors)
+		{
+			float rx = width / 2 - 1f;
+			float ry = height - 1f;
+
+			float complementaryHue = Mathf.Repeat(hue + 0.5f, 1f);
+
+			for (int y = 0, i = 0; y < height; ++y)
+			{
+				float fy = y / ry;
+				for (int x = 0; x < width; ++x, ++i)
+				{
+					float fx = (x - rx) / rx;
+					if (fx < 0f)
+					{
+						var hcl = new ColorHCL(hue, -fx, fy);
+						hcl.a = hcl.canConvertToRGB ? 1f : 0f;
+						colors[i] = (Color)hcl;
+					}
+					else
+					{
+						var hcl = new ColorHCL(complementaryHue, fx, fy);
+						hcl.a = hcl.canConvertToRGB ? 1f : 0f;
+						colors[i] = (Color)hcl;
+					}
+				}
+			}
+		}
+
+		private static void CalculateHueSliceHSY(float hue, int width, int height, Color[] colors)
+		{
+			float rx = width / 2 - 1f;
+			float ry = height - 1f;
+
+			float complementaryHue = Mathf.Repeat(hue + 0.5f, 1f);
+
+			for (int y = 0, i = 0; y < height; ++y)
+			{
+				float fy = y / ry;
+				for (int x = 0; x < width; ++x, ++i)
+				{
+					float fx = (x - rx) / rx;
+					if (fx < 0f)
+					{
+						var hsy = new ColorHSY(hue, -fx, fy);
+						hsy.a = 1f;
+						colors[i] = (Color)hsy;
+					}
+					else
+					{
+						var hsy = new ColorHSY(complementaryHue, fx, fy);
+						hsy.a = 1f;
+						colors[i] = (Color)hsy;
+					}
+				}
+			}
 		}
 
 		private static void CalculateHueSliceHCY(float hue, int width, int height, Color[] colors)
@@ -39,23 +184,23 @@ namespace Experilous.Examples.MakeItColorful
 			float rx = width / 2 - 1f;
 			float ry = height - 1f;
 
-			float complementaryHue = Mathf.Repeat(hue + Mathf.PI, Mathf.PI * 2f);
+			float complementaryHue = Mathf.Repeat(hue + 0.5f, 1f);
 
 			for (int y = 0, i = 0; y < height; ++y)
 			{
-				float fy = (height - y) / ry;
+				float fy = y / ry;
 				for (int x = 0; x < width; ++x, ++i)
 				{
 					float fx = (x - rx) / rx;
 					if (fx < 0f)
 					{
-						var hcy = new ColorHCY(hue, fx, fy);
+						var hcy = new ColorHCY(hue, -fx, fy);
 						hcy.a = hcy.canConvertToRGB ? 1f : 0f;
 						colors[i] = (Color)hcy;
 					}
 					else
 					{
-						var hcy = new ColorHCY(complementaryHue, -fx, fy);
+						var hcy = new ColorHCY(complementaryHue, fx, fy);
 						hcy.a = hcy.canConvertToRGB ? 1f : 0f;
 						colors[i] = (Color)hcy;
 					}
